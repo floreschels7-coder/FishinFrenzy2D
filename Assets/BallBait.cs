431462eae9db2b89a264bd97d909f742f3acb9fa
floreschels7-coder <floreschelsey7@gmail.com>
2025-11-13 10:53:03 -0500

    created the fish tag for the fish and added the boxcollider

--- Assets/BallBait.cs
+++ Assets/BallBait.cs
@@ -13,18 +13,18 @@ public class BallBait : MonoBehaviour
         Destroy(gameObject, timeOfBall);
             }
 
-    // void OnTriggerEnter2D(Collider2D otherObject)
-    // {
-    //     //if this 'otherObject' falls into the "Fish" category then...
-    //     if (otherObject.CompareTag("Fish"))
-    //         {
-    //             //destory this 'otherObject'
-    //             Destroy(otherObject.gameObject);
+    void OnTriggerEnter2D(Collider2D otherObject)
+    {
+        //if this 'otherObject' falls into the "Fish" category then...
+        if (otherObject.CompareTag("Fish"))
+            {
+                //destory this 'otherObject'
+                Destroy(otherObject.gameObject);
 
-    //             //That is essentially earning 1 point that will get added to the total score
-    //             // totalScore.instance.AddToScore(1);
+                //That is essentially earning 1 point that will get added to the total score
+                // totalScore.instance.AddToScore(1);
 
-    //             Destroy(gameObject);
-    //         }
-    // }
+                Destroy(gameObject);
+            }
+    }
 }

df78262a7502a0381e25228f9a5c5f02db8e823e
floreschels7-coder <floreschelsey7@gmail.com>
2025-12-14 20:06:46 -0500

    fixed a bug in timer class

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
@@ -37,7 +38,7 @@ public class Timer : MonoBehaviour
             {
                 //subtract time and time.deltatime is the
                 //  amt of time passed since the last frame
-                time -= Timer.deltaTime;
+                time -= Time.deltaTime;
 
                 //update text on screen 
                 //Mathf rounds to nearest whole number
